74c379421a486f9ad28e234ef80a3c2370bf6e7e
sarahcordoba <sarahcordobasalgado@gmail.com>
2021-11-26 17:02:53 -0500

    entidades

---
+++ Hackaton.App.Dominio/Entidades/Entidad_colaborativa.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Hackaton.App.Dominio{
+    public class Entidad_colaborativa{
+        public int id { get; set; }
+        public string razon_social { get; set; }
+        public string nit { get; set; }
+        public string direccion { get; set; }
+        public string ciudad { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string pagina_web { get; set; }
+ }
+}
